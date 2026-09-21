5e9cc72715fb58b2e3252fc4dc304d18549094db
Kalebe Sant' Ana <82844427+kao04@users.noreply.github.com>
2026-09-21 08:31:08 -0300

    asd

--- frotend/script.js
+++ frotend/script.js
@@ -9,10 +9,10 @@ async function buscarFilmes() {
     filmes.forEach((filme) => {
         lista.innerHTML += `
 		<div>
-        <h2>${filme.title}</h2>
-        <p><strong>Gênero: ${filme.gender}</strong></p>
-        <p><strong>Duração: ${filme.duration}</strong></p>
-        <p><strong>Clasificação: ${filme.classificacao > "0" ? filme.ageLimit + 'anos' : 'livre'}</strong></p>
+        <h2>${filme.name}</h2>
+        <p><strong>Gênero: ${filme.genero}</strong></p>
+        <p><strong>Duração: ${filme.duracao}</strong></p>
+        <p><strong>Clasificação: ${filme.classificacao > "0" ? filme.classificacao + 'anos' : 'livre'}</strong></p>
         </div>
                 `
         console.log(filme)
